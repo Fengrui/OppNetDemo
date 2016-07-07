89f3ca28c874c655f6a60b60ad136e3ffa4e9f27
Fengrui <arthur.shifengrui@gmail.com>
2016-07-07 18:13:24 +0100

    v2 development

--- OppNetDemo1/src/info/fshi/oppnetdemo1/log/PeerLog.java
+++
@@ -1,9 +0,0 @@
-package info.fshi.oppnetdemo1.log;
-
-public class PeerLog {
-	public int dir; // 0=send, 1=receive
-	public String mac;
-	public int qLen;
-	public long sTimestamp;
-	public long eTimestamp;
-}
--- OppNetDemo1/src/info/fshi/oppnetdemo1/log/PeerLogList.java
+++
@@ -1,9 +0,0 @@
-package info.fshi.oppnetdemo1.log;
-
-import java.util.ArrayList;
-
-public class PeerLogList {
-
-	public static ArrayList<PeerLog> peerLogList = new ArrayList<PeerLog>();
-	
-}
